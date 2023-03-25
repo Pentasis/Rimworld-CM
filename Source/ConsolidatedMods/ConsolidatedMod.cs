7edc392a521c27912154e036978dbb32ee4da226
Pentasis <7046024+Pentasis@users.noreply.github.com>
2023-03-25 12:42:57 +0100

    Add project files.

---
+++ Source/ConsolidatedMods/ConsolidatedMod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace ConsolidatedMods {
+
+    public class ConsolidatedMod : Mod {
+
+        public ConsolidatedMod(ModContentPack content) : base(content) {
+            Harmony harmony = new Harmony("mod.pentasis.consolidatedmods");
+
+        }
+    }
+}
